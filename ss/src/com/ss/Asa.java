ddcdd2cad1b54976940f1dd8209f15522b6814e9
chenyin123 <2823644108@qq.com>
2019-03-12 14:17:19 +0800

    asd

---
+++ ss/src/com/ss/Asa.java
@@ -0,0 +1,22 @@
+package com.ss;
+
+public class Asa {
+	public static void main(String[] args) {
+	    int one = 10 ;
+        int two = 20 ;
+        int three = 0 ;
+        three=one+two;
+        System.out.println("three = one + two ==> "+three);
+        three+=one;
+        System.out.println("three += one ==> "+three);
+        three-=one;
+        System.out.println("three -= one ==> "+three);
+        three*=one;
+        System.out.println("three *= one ==> "+three);
+        three/=one;
+        System.out.println("three /= one ==> "+three);
+        three%=one;
+        System.out.println("three %= one ==> "+three);
+}
+
+}
